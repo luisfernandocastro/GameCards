31783a991283f1e2153679a4af7613420476ff17
luisfernandocastro <rockstartplus@gmail.com>
2021-10-22 15:55:43 -0500

    commit Ultim

--- core/Messages/apps.py
+++ core/Messages/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
-
+#aplicacion de mensajes 
 class MessagesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core.Messages'
